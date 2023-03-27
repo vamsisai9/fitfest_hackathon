63d3da0740c3d7c4bd2e67d52b25dcaf771c6de4
vamsisai9 <vamsi.sai.work@gmail.com>
2023-03-28 02:00:23 +0530

    Update test4.py

--- test4.py
+++ test4.py
@@ -10,6 +10,28 @@ thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
 # Perform connected component analysis to isolate text regions
 nLabels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh, connectivity=8)
+def calculate_boldness(region):
+    # Convert the region to grayscale
+    gray = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
+
+    # Compute the Scharr gradient magnitude representation of the images in both the x and y direction
+    grad_x = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+    grad_y = cv2.Sobel(gray, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1)
+
+    # Compute the magnitude of the gradient
+    mag = cv2.magnitude(grad_x, grad_y)
+
+    # Compute the mean intensity of the image
+    mean_intensity = np.mean(mag)
+
+    # Compute the standard deviation of the image intensity
+    std_intensity = np.std(mag)
+
+    # Compute the ratio of standard deviation to mean intensity as the boldness
+    boldness = std_intensity / mean_intensity
+
+    return boldness
+
 
 # Loop over the regions and extract the text
 for i in range(1, nLabels):
